d790de6f1bddd58fdab3ed267a6c56c6ade04cbe
astragenius <darthmalak86@gmail.com>
2021-10-07 18:23:47 +0200

    html markup added

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 const { src, dest, watch ,series } = require('gulp')
 const sass = require('gulp-sass')(require('sass')) 
+const autoprefixer = require('gulp-autoprefixer');
+
+
+
 
 function buildStyles() {
 
@@ -13,5 +17,7 @@ function watchTask() {
 
     watch(['scss/**/*.scss'], buildStyles)
 }
+
+
 
 exports.default = series(buildStyles, watchTask)
